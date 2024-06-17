1d9422117e3dfaee27cd04c47c132ab1bf2168a6
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-06-17 20:20:24 +0200

    Add stats functions

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ pub struct RollingStats {
     window_size: usize,
     endianness: Endianness,
     values: VecDeque<i32>,
+    sum: i64,
+    sum_of_squares: i64,
 }
 
 #[derive(Debug)]
@@ -12,7 +14,6 @@ pub enum Endianness {
     Little,
     Big,
 }
-
 
 impl RollingStats {
     pub fn new(window_size: usize, endianness: Endianness) -> Self {
@@ -22,12 +23,35 @@ impl RollingStats {
             window_size,
             endianness,
             values: VecDeque::with_capacity(window_size),
+            sum: 0,
+            sum_of_squares: 0,
         }
     }
     pub fn add_sample(&mut self, value: i32) {
         if self.values.len() == self.window_size {
-            self.values.pop_front();
+            if let Some(removed) = self.values.pop_front() {
+                self.sum -= removed as i64;
+                self.sum_of_squares -= (removed as i64).pow(2);
+            }
         }
         self.values.push_back(value);
+        self.sum += value as i64;
+        self.sum_of_squares += (value as i64).pow(2);
+    }
+    pub fn get_mean(&self) -> f32 {
+        let count = self.values.len();
+        if count == 0 {
+            return 0.0;
+        }
+        self.sum as f32 / count as f32
+    }
+    pub fn get_std_dev(&self) -> f32 {
+        let count = self.values.len();
+        if count < 2 {
+            return 0.0;
+        }
+        let mean = self.get_mean();
+        let variance = (self.sum_of_squares as f32 / count as f32) - (mean * mean);
+        variance.sqrt()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,19 @@ use rolling_stats::{RollingStats,Endianness};
 fn main() {
     println!("Creating empty stats with size 2");
     let mut stats: RollingStats = RollingStats::new(2, Endianness::Big);
+    println!("Stats for empty struct: {} {}", stats.get_mean(), stats.get_std_dev());
     println!("{:?}", stats);
     println!("Adding 10, expecting 1 element");
     stats.add_sample(10);
     println!("{:?}", stats);
+    println!("Stats: {} {}", stats.get_mean(), stats.get_std_dev());
     println!("Adding -20, expecting 2 elements");
     stats.add_sample(-20);
     println!("{:?}", stats);
+    println!("Stats: {} {}", stats.get_mean(), stats.get_std_dev());
     println!("Adding 5, expecting 2 elements: -20 and 5");
     stats.add_sample(5);
     println!("{:?}", stats);
+    println!("Stats: {} {}", stats.get_mean(), stats.get_std_dev());
 
 }
